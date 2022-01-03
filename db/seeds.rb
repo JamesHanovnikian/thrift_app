3e96473bca61f4804088c0bc32d67864438dc492
YOUR NAME <jhanov@gmail.com>
2022-01-02 19:18:59 -0600

    seed database with sample clothes

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,13 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Product.create(name: "Blue Flannel Button Down", price: 20, category: "tops", size: "Medium")
+Product.create(name: "Levi's Blue Jeans", price: 15, category: "Pants", size: "Large")
+Product.create(name: "Gucci Fedora", price: 20, category: "Accessories", size: "One")
+Product.create(name: "Cowboy Boots", price: 40, category: "Shoes", size: "12")
+Product.create(name: "Leather Jacket", price: 100, category: "Outerwear", size: "Large")
+Product.create(name: "Bonobs Red Button Down", price: 15, category: "tops", size: "Small")
+Product.create(name: "North Face Jacket", price: 75, category: "Small", size: "Small")
+Product.create(name: "Dickies Work Jacket", price: 50, category: "Outerwear", size: "Large")
+Product.create(name: "Vintage Nirvana Shirt", price: 40, category: "tops", size: "Medium")
