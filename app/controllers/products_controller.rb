7cb8be7485e9179391d9e75baa974dddd7b84fc7
YOUR NAME <jhanov@gmail.com>
2022-01-05 15:56:18 -0600

    adds all rest actions for products controller

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,2 +1,38 @@
 class ProductsController < ApplicationController
+  def index
+    products = Product.all
+    render json: products.as_json
+  end
+
+  def create
+    product = Product.new(
+      name: params[:name],
+      price: params[:price],
+      category: params[:category],
+      size: params[:size],
+    )
+    product.save
+    render json: product.as_json
+  end
+
+  def show
+    product = Product.find_by(id: params[:id])
+    render json: photo.as_json
+  end
+
+  def update
+    product = Product.find_by(id: params[:id])
+    product.name = params[:name] || product.name
+    product.price = params[:price] || product.price
+    product.category = params[:category] || product.category
+    product.size = params[:size] || product.size
+    product.save
+    render json: product.as_json
+  end
+
+  def destroy
+    product = Product.find_by(id: params[:id])
+    product.destroy
+    render json: "Product is succesfully removed"
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,11 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
+  # Products Routes
+
+  get "/products" => "products#index"
+  post "/products" => "products#create"
+  get "/products/:id" => "products#show"
+  patch "/products/:id" => "products#update"
+  delete "/products/:id" => "products#destroy"
 end
