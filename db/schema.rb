abb1ea71a8248dabb8ca8d3ffcfc75df96e95b7a
YOUR NAME <jhanov@gmail.com>
2022-01-05 18:57:09 -0600

    adds order model, controller

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_01_05_215843) do
+ActiveRecord::Schema.define(version: 2022_01_06_004943) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "orders", force: :cascade do |t|
+    t.integer "user_id"
+    t.integer "subtotal"
+    t.integer "tax"
+    t.integer "total"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "products", force: :cascade do |t|
     t.string "name"
